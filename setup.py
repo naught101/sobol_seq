94ac9b68eab4eb5ae5c6d73f19402e7f94f79670
naught101 <naught101@gmail.com>
2015-08-17 17:21:26 +1000

    setuptools for your leisure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sobol_seq',
+      version='0.1',
+      description='Sobol sequence generator',
+      url='http://github.com/storborg/funniest',
+      author='naught101',
+      author_email='naught101@gmail.com',
+      license='MIT',
+      packages=['sobol_seq'],
+      zip_safe=False)
